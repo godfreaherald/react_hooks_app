6649569a86e3735c7f289313bb0c93b6a3a92cfd
godfreaherald <godfreywambua@GODFREYs-MacBook-Pro.local>
2019-06-20 13:25:24 +0300

    Updates

---
+++ precache-manifest.88307ca00666664c44078cd03c181203.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3453595707b4bbb8d78c043167521335",
+    "url": "/react_hooks_app/index.html"
+  },
+  {
+    "revision": "d28227f738a9532ba5cb",
+    "url": "/react_hooks_app/static/css/main.0c1ba5b3.chunk.css"
+  },
+  {
+    "revision": "dded7ebfa7b12581548a",
+    "url": "/react_hooks_app/static/js/2.27fbf297.chunk.js"
+  },
+  {
+    "revision": "d28227f738a9532ba5cb",
+    "url": "/react_hooks_app/static/js/main.9a03a407.chunk.js"
+  },
+  {
+    "revision": "2de3e8d5a0ef12950339",
+    "url": "/react_hooks_app/static/js/runtime~main.7216acd7.js"
+  }
+]);
